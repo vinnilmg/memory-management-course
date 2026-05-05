146a20f5850904755ee00857124bfc493882e0cd
Vinicius Gomes <vini.delimagomes@gmail.com>
2026-05-04 21:22:00 -0300

    modulo 3 - inicio

--- src/main/java/med/voll/api/memory/tests/MainTests.java
+++ src/main/java/med/voll/api/memory/tests/MainTests.java
@@ -1,11 +1,16 @@
 package med.voll.api.memory.tests;
 
+import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.paciente.Paciente;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class MainTests {
     public static void main(String[] args) {
         // modulo1();
         // modulo2();
+        modulo3();
     }
 
     private static void modulo1() {
@@ -23,5 +28,14 @@ public class MainTests {
 
         final var valor3 = new String("Alura");
         System.out.println(valor1 == valor3); // false
+    }
+
+    private static void modulo3() {
+        final var consultas = new ArrayList<Consulta>();
+        final var consulta = new Consulta();
+
+        // Forçar o OutOfMemoryError
+        IntStream.range(1, 1000000000)
+                .forEach(i -> consultas.add(consulta));
     }
 }
